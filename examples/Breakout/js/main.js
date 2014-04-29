39121dd118240c4269f2bba226ea2fcddfbec440
Rosen Dobrev <r.dobrev92@gmail.com>
2014-04-29 10:44:06 +0300

    final

--- examples/Breakout/js/main.js
+++ examples/Breakout/js/main.js
@@ -1,14 +1,21 @@
-var game = new Phaser.Game(680, 480, Phaser.AUTO, '', {preload: preload, create: create, update: update });
+w = window.innerWidth;
+h = window.innerHeight;
+var game = new Phaser.Game(w, h, Phaser.AUTO, '', {preload: preload, create: create, update: update });
 
 /* variables for ball, pad and bricks */
 var ball;
 var paddle;
 var bricks;
 var brick_array = new Array(60);
+
+//variable for bricks who has power-ups in it
 var pu = new Array();
+
+//variable for all power-ups
 var powerups;
 
- var powerup;
+//variable for catched power-up
+var powerup;
 
 //index variable
 var index = 0;
@@ -28,6 +35,7 @@ var introText;
 /* variable for... Something? */
 var s;
 
+var paddleSize;
 function preload(){
     game.load.image('background', 'assets/background.jpg');
     game.load.image('ball','assets/ball.png');
@@ -38,12 +46,12 @@ function preload(){
     game.load.image('brick3', 'assets/brick3.png');
     game.load.image('brick4', 'assets/brick4.png');
     game.load.image('brick5', 'assets/brick5.png');
-    game.load.image('power-up', 'assets/power-up.jpg');
+    game.load.image('power-up', 'assets/phpDqhck1AM.jpg');
 }
 
 function create(){
 
-    s = game.add.tileSprite(0, 0, 680, 480, 'background');
+    s = game.add.tileSprite(0, 0, w, h, 'background');
 
     bricks = game.add.group();
     bricks.enableBody = true;
@@ -56,39 +64,41 @@ function create(){
 
     var brick;
     var inc = 0;
-    //var brick_array = new Array(60);
 
-    for(var y = 0; y < 4; y++){
+    for(var y = 0; y < 5; y++){
         for(var x = 0; x < 15; x++){
             inc = inc + 1;
-            brick_array[inc] = bricks.create(120 + (x * 36), 100 + (y * 52), 'brick' + (y + 1));
+            brick_array[inc] = bricks.create(w/5 + (x * (w*(4/100))), h/4 + (y * (h*(6/100))), 'brick' + (y + 1));
             brick_array[inc].body.bounce.set(1);
             brick_array[inc].body.immovable = true;
             //brick_array[inc] = brick;
+            brick_array[inc].width = w * (3/100);
         }
     }
-    //array for index of bricks with power-ups
 
-    for(var i = 0; i < (brick_array.length*(15/100)); i++){
+    //array for index of bricks with power-ups
+    for(var i = 0; i < (brick_array.length*(90/100)); i++){
     pu[i] = brick_array[Math.floor((Math.random() * brick_array.length) + 1)];
     }
 
 
-    paddle = game.add.sprite(game.world.centerX, 400, 'paddle');
-    paddle.anchor.setTo(0.5, 0.5);
+    paddle = game.add.sprite(w*(50/100), h*(90/100), 'paddle');
     //paddle.scale.x = 2;
 
-
     game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    //paddle.scale.x = 2;
+    paddle.anchor.setTo(0.5,0.5);
+
 
     paddle.body.collideWorlBounds = true;
     paddle.body.bounce.set(1);
     paddle.body.immovable = true;
 
-    ball = game.add.sprite(paddle.body.x + 100, paddle.body.y - 10, 'ball');
-    ball.anchor.set(0.5);
+    paddleSize = game.cache.getImage("paddle");
 
-    paddle.y-16
+    ball = game.add.sprite(paddle.body.x + 100, paddle.body.y - 10, 'ball');
+    ball.anchor.setTo(0.5, 0.5);
+
     ball.checkWorldBounds = true;
 
     game.physics.enable(ball, Phaser.Physics.ARCADE);
@@ -98,25 +108,25 @@ function create(){
 
     ball.events.onOutOfBounds.add(ballLost, this);
 
-    scoreText = game.add.text(32, 30, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    livesText = game.add.text(600, 30, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    introText = game.add.text(game.world.centerX - 100, 300, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+    scoreText = game.add.text(w*(5/100), h*(5/100), 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    livesText = game.add.text(w*(90/100), h*(5/100), 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    introText = game.add.text(w*(35/100), h*(60/100), '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
     game.input.onDown.add(releaseBall, this);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.checkCollision.down = false;
-
 }
+
 function update(){
-    paddle.body.x = game.input.x - 30;
+    paddle.body.x = game.input.x;
 
     if (paddle.body.x < 0)
     {
         paddle.body.x = 0;
     }
-    else if (paddle.body.x > game.width - 24)
+    else if (paddle.body.x > w-paddleSize.width)
     {
-        paddle.body.x = game.width - 24;
+        paddle.body.x = w-paddleSize.width;
     }
 
     if(ballOnPaddle){
@@ -140,10 +150,14 @@ function releaseBall(){
 function ballLost(){
     lives--;
     livesText.text = 'lives: ' + lives;
+    paddle.scale.x = 1;
+    paddle.width = w * (6/100);
+    paddle.body.width = w * (6/100);
+    //paddle.reset(w/2, h*(90/100));
+    //powerups.alive.kill();
     if(powerup){
         powerup.kill();
     }
-    //paddle.scale.x = 1.0;
     if(lives === 0){
         gameOver();
     }else{
@@ -153,6 +167,7 @@ function ballLost(){
 }
 
 function gameOver(){
+    ballOnPaddle = false;
     ball.body.velocity.setTo(0,0);
 
     introText.text = 'Game Over, Neo..';
@@ -171,8 +186,6 @@ function ballHitBrick (_ball, _brick) {
             if(_brick == pu[i]){
 
                 powerup = powerups.create(pu[i].x, pu[i].y, 'power-up');
-                powerup.scale.x = 0.1;
-                powerup.scale.y = 0.1;
                 game.physics.enable(powerup, Phaser.Physics.ARCADE);
                 powerup.body.gravity.y = 50;
             }
@@ -195,11 +208,9 @@ function ballHitBrick (_ball, _brick) {
         //  And bring the bricks back from the dead :)
         bricks.callAll('revive');
     }
-
 }
 
 function ballHitPaddle (_ball, _paddle) {
-
     var diff = 0;
 
     if (_ball.x < _paddle.x)
@@ -220,12 +231,15 @@ function ballHitPaddle (_ball, _paddle) {
         //  Add a little random X to stop it bouncing straight up!
         _ball.body.velocity.x = 2 + Math.random() * 8;
     }
-
 }
 
 function catchPowerUp(_paddle, _powerup){
-    var a;
     _powerup.kill();
+    score += 100;
+    scoreText.text = 'score: ' + score;
+
     _paddle.scale.x = 2;
-    game.physics.enable(_paddle, Phaser.Physics.ARCADE);
+
+    //_paddle.reset(w/2, h*(90/100));
+    _paddle.body.width = w*(10/100);
 }
