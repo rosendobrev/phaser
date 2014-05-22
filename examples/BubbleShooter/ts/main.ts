bd5a4484c188df69ea82b49ac00bf5cb7a61196f
Rosen Dobrev <r.dobrev92@gmail.com>
2014-05-22 12:04:41 +0300

    init commit

---
+++ examples/BubbleShooter/ts/main.ts
@@ -0,0 +1,10 @@
+w = window.innerWidth;
+h = window.innerHeight;
+
+var game = new Phaser.Game(w, h, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
+function preload(){}
+
+function create(){}
+
+function update(){}
